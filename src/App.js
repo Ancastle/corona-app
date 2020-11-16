336e56a7fe1666ddb7fa63bf71afc9ee44718120
Jorge <J-anaya-@hotmail.com>
2020-11-16 14:28:38 -0500

    Using Material UI Cards

--- src/App.js
+++ src/App.js
@@ -5,20 +5,27 @@ import styles from './App.module.css'
 import { fetchData } from './api' // You can call index.js files like this
 
 class App extends React.Component {
+    state = {
+        data: {}
+    };
+
     async componentDidMount () { //Best place to do the request for an api
-        const data = await fetchData();
-        console.log(data);
-    }
+        const fetchedData = await fetchData();
+        this.setState({data : fetchedData});
+    };
+
     render() {
+        const { data } = this.state;
+
         return (
-            <div className={styles.container}>
-                <Cards/>
+            <div className={ styles.container }>
+                <Cards data={ data }/>
                 <CountryPicker/>
                 <Chart/>
             </div>
-        )
+        );
 
-    }
+    };
 }
 
 export default App;
--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,11 @@
-import axios from 'axios';
+import axios from 'axios'; // Axios is a popular, promise-based HTTP client that sports an easy-to-use API and can be used in both the browser and Node. js.
 
 const url = 'https://covid19.mathdro.id/api';
 
 export const fetchData = async () => {
     try {
-        const response = await axios.get(url);
-        return response;
+        const { data: {confirmed, recovered, deaths, lastUpdate} } = await axios.get(url);
+        return { confirmed, recovered, deaths, lastUpdate };
     } catch (error) {
         
     }
